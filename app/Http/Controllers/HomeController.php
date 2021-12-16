c1453d5a76e82fcba9854430bc6f0cade127ff0d
root <root@ip-172-31-84-73.ec2.internal>
2021-12-16 05:42:47 +0000

    mntp

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,8 @@ class HomeController extends Controller
     public function index() {
         if (Auth::check()) {
             return redirect()->route('main.timeline.index');
-        }
+	}
+
+	return "Hello world!";
     }
 }
